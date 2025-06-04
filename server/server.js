55086fe9deb670ac4c9c9de28a40c7cabeb807b8
Naman <165200334+namanp09@users.noreply.github.com>
2025-06-04 22:24:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   }); 
-app.post('./webhooks',clerkWebhooks)
+app.post('/webhooks',clerkWebhooks)
 
 
 
