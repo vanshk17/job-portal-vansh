832a4677ff85f655dd847875069d90fb9a049640
Naman <naman.padiyar09@gmail.com>
2025-06-05 19:01:22 +0530

    7:52

--- server/controllers/companyConroller.js
+++ server/controllers/companyConroller.js
@@ -78,8 +78,16 @@ export const loginCompany = async (req, res) => {
 
 // Get company data
 export const getCompanyData = async (req, res) => {
-  // Logic to get company data goes here
-};
+    try {
+        const company = req.company
+        
+        res.json({success:true, company})
+    } catch (error) {
+        res.json({
+            success:false,message:error.message
+        })
+    }
+}
 // Post a new job
 export const postJob = async (req, res) => {
   const { title, description, location, salary,level,category} = req.body;
@@ -114,11 +122,37 @@ export const getCompanyJobApplicants = async (req, res) => {
 
 // Get Company Posted Jobs
 export const getCompanyPostedJobs = async (req, res) => {
-  // Logic to get posted jobs goes here
-};
+    try {
+        const companyId = req.company._id
+        
+        const jobs = await Job.find({companyId})
+        
+        // (ToDo) Adding No. of applicants info in data
+        
+        res.json({success:true, jobsData: jobs})
+    } catch (error) {
+        res.json({ success:false, message: error.message})
+    }
+}
 export const changeJobApplicationsStatus = async (req, res) => {
   // Logic to get posted jobs goes here
 };
 export const changeVisibility = async (req, res) => {
-  // Logic to get posted jobs goes here
-};
+    try {
+        const {id} = req.body
+        
+        const companyId = req.company._id
+        
+        const job = await Job.findById(id)
+        
+        if (companyId.toString() === job.companyId.toString()) {
+            job.visible = !job.visible
+        }
+        
+        await job.save()
+        
+        res.json({success:true,job})
+    } catch (error) {
+        res.json({ success:false, message: error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -8,12 +8,15 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js';
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 await connectDB()
 await connectCloudinary()
@@ -38,6 +41,7 @@ app.post('/webhooks', clerkWebhooks)
 
 app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 
 // Port
