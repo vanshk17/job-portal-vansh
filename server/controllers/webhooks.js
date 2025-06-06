3d7b5f532c82a195e91d8b270b95b2ebcc086c03
Naman <naman.padiyar09@gmail.com>
2025-06-07 01:46:53 +0530

    a

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,12 +1,14 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
+
 // API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
    try {
        
        // Create a Svix instance with clerk webhook secret.
        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+       
        
        // Verifying Headers
        await whook.verify(JSON.stringify(req.body), {
